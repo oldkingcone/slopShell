c3f2f09acaf406a239caf4b27227dff4ac788d13
oldkingcone <no.no@no.com>
2021-02-10 12:00:48 -0500

    Update postgres_checker.php

--- includes/db/postgres_checker.php
+++ includes/db/postgres_checker.php
@@ -14,8 +14,10 @@ class postgres_checker
             return false;
         }
         try {
+            # for obvious security, removing select role from sloppy_main.
+            # will create a function in this class to handle query of records and to return that record.
             pg_exec($this->connectionString, "CREATE TABLE IF NOT EXISTS sloppy_bots_main(id SERIAL NOT NULL constraint sloppy_bots_main_pkey primary key,datetime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, rhost TEXT, uri TEXT, os_flavor TEXT NOT NULL DEFAULT '-', check_in INTEGER NOT NULL default 0)");
-            pg_exec($this->connectionString, "GRANT SELECT, INSERT, UPDATE ON TABLE sloppy_bots TO sloppy_main");
+            pg_exec($this->connectionString, "GRANT INSERT,UPDATE ON TABLE sloppy_bots_main TO sloppy_main");
             return true;
         }catch (Exception $e){
             $this->er = $e;
@@ -26,12 +28,19 @@ class postgres_checker
 
     public function checkDB(){
         # the idea for this, is to check to see if the db returns 1 record. if it does not, we will call create db, or start db.
-        $this->connectionString = pg_connect("host=localhost port=5432 user=sloppy_main dbname=sloppy_bots");
+        $this->connectionString = pg_connect("host=localhost port=5432 user=postgres dbname=sloppy_bots");
         try {
             pg_exec($this->connectionString, 'SELECT id FROM sloppy_bots WHERE id IS 1');
             return true;
         }catch (Exception $ee) {
             return false;
+        }
+    }
+
+    public function getRecord($ip){
+        if (!empty($ip) && gettype($ip) === 'string'){
+
+
         }
     }
 }
