26103df8787e37bfa4402678ce8fb5c29f7b21ca
oldkingcone <no.no@no.com>
2021-03-28 02:22:24 -0400

    Update dynamic_generator.php

--- includes/droppers/dynamic_generator.php
+++ includes/droppers/dynamic_generator.php
@@ -44,7 +44,7 @@ class dynamic_generator
                 $a = "// why is it not launching??????\n";
                 break;
             case 15:
-                $yy = rand(1997,date("Y"));
+                $yy = rand(1997,(int)date("Y"));
                 $mo = rand(1,12);
                 $dd = rand(1,31);
                 $a = "//created: \n". date("Y/m/d - l", mktime(null, null, null, $mo, $dd, $yy));
